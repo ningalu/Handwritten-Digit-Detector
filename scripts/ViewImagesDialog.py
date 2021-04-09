ccf650d9d557dae52afb5edd53dca4ac2875f018
sdiv877 <sdiv877@aucklanduni.ac.nz>
2021-04-09 15:29:44 +1200

    Added previous and next buttons for image nav

--- scripts/ViewImagesDialog.py
+++ scripts/ViewImagesDialog.py
@@ -4,7 +4,8 @@ from math import ceil
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, qApp, QHBoxLayout, QVBoxLayout
 from PyQt5.QtWidgets import QDialog
-from PyQt5.QtWidgets import QPushButton, QTextEdit, QProgressBar
+from PyQt5.QtWidgets import QPushButton, QTextEdit, QProgressBar, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
 
 import time
 import matplotlib.pyplot as plt
@@ -21,9 +22,17 @@ class ViewImagesDialog(QDialog):
         self.title = title
         self.dataset = dataset
 
+        if ('train' in self.title.lower()):
+            self.dirString = './images/train'
+        else:
+            self.dirString = './images/test'
+
         self.imageList = []
         self.createImageList()
         self.saveImages()
+
+        self.imageLabel = 0
+        self.viewImageIndex = 0
 
         self.initUI()
 
@@ -33,22 +42,53 @@ class ViewImagesDialog(QDialog):
         self.createDialogLayout()
 
         self.centreWindow()
+        self.show()
 
     def centreWindow(self):
         # qr = self.frameGeometry()
         # cp = QDesktopWidget().availableGeometry().center()
         # qr.moveCenter(cp)
         # self.move(qr.topLeft())
-        self.resize(320, 180)
+        # self.resize(320, 180)
+        pass
 
     def createWidgetLayouts(self):
-        pass
+        # Create imageLabel and set the default image to 1, 100
+        self.imageLabel = QLabel()
+        pixmap = QPixmap(f'{self.dirString}/1,100.png')
+        self.imageLabel.setPixmap(pixmap)
+
+        # Create imageLayout and add the imageLabel to it
+        self.imageLayout = QVBoxLayout()
+        self.imageLayout.addWidget(self.imageLabel)
+
+        # Create the buttons needed
+        self.previousButton = QPushButton('Previous')
+        self.previousButton.clicked.connect(self.prevViewImage)
+        self.nextButton = QPushButton('Next')
+        self.nextButton.clicked.connect(self.nextViewImage)
+
+        # selectButton = QPushButton('Select image')
+        # menu = QMenu(self)
+        # menu.addAction('First Item')
+        # menu.addAction('Second Item')
+        # menu.addAction('Third Item')
+        # menu.addAction('Fourth Item')
+        # popupbutton.setMenu(menu)
+
+        # Create buttonLayout and add the buttons to it
+        self.buttonLayout = QVBoxLayout()
+        self.buttonLayout.addWidget(self.previousButton)
+        self.buttonLayout.addWidget(self.nextButton)
+        # self.buttonLayout.addWidget(selectButton)
 
     def createDialogLayout(self):
         # Create a main HBoxLayout
         self.mainHBoxLayout = QHBoxLayout()
 
         # Add our widget layouts to the mainHLayout
+        self.mainHBoxLayout.addLayout(self.imageLayout)
+        self.mainHBoxLayout.addLayout(self.buttonLayout)
 
         # Add mainHLayout to this classes layout
         self.setLayout(self.mainHBoxLayout)
@@ -59,17 +99,9 @@ class ViewImagesDialog(QDialog):
             currImage, _ = self.dataset[i]
             self.imageList.append(currImage)
 
-        # plt.imshow(self.imageList[1], cmap='gray')
-        # plt.show()
-
     def saveImages(self):
-        if ('train' in self.title.lower()):
-            dirString = './images/train'
-        else:
-            dirString = './images/test'
-
-        if not path.exists(dirString):
-            makedirs(dirString)
+        if not path.exists(self.dirString):
+            makedirs(self.dirString)
 
         for i in range(0, 100):
             npimg = torchvision.utils.make_grid(
@@ -77,5 +109,27 @@ class ViewImagesDialog(QDialog):
             npimg = npimg.numpy()
             npimg = np.transpose(npimg, (1, 2, 0))
 
-            plt.imsave(f'{dirString}/{i*100},{(i*100) + 100}.png', npimg)
-            # plt.imshow(np.transpose(npimg, (1, 2, 0)))
+            plt.imsave(
+                f'{self.dirString}/{i*100 + 1},{(i*100) + 100}.png', npimg)
+
+    def prevViewImage(self):
+        if (self.viewImageIndex > 0):
+            self.viewImageIndex -= 1
+        else:
+            self.viewImageIndex = 99
+
+        pixmap = QPixmap(
+            f'{self.dirString}/{self.viewImageIndex*100 + 1},{(self.viewImageIndex*100) + 100}.png')
+        self.imageLabel.setPixmap(pixmap)
+        QApplication.processEvents()
+
+    def nextViewImage(self):
+        if (self.viewImageIndex < 99):
+            self.viewImageIndex += 1
+        else:
+            self.viewImageIndex = 0
+
+        pixmap = QPixmap(
+            f'{self.dirString}/{self.viewImageIndex*100 + 1},{(self.viewImageIndex*100) + 100}.png')
+        self.imageLabel.setPixmap(pixmap)
+        QApplication.processEvents()
--- scripts/test.py
+++ scripts/test.py
@@ -1,20 +1,93 @@
-import time
-import matplotlib.pyplot as plt
-import numpy as np
+# import time
+# import matplotlib.pyplot as plt
+# import numpy as np
 
-from torchvision import datasets, transforms
+# from torchvision import datasets, transforms
 
-dataset = datasets.MNIST(
-    root='./mnist_data',
-    download=True,
-    transform=transforms.ToTensor()
-)
+# dataset = datasets.MNIST(
+#     root='./mnist_data',
+#     download=True,
+#     transform=transforms.ToTensor()
+# )
 
-start = time.time()
+# start = time.time()
 
-x, _ = dataset[7777] # x is now a torch.Tensor
-plt.imshow(x.numpy()[0], cmap='gray')
+# x, _ = dataset[7777] # x is now a torch.Tensor
+# plt.imshow(x.numpy()[0], cmap='gray')
 
-end = time.time()
-print(f'it took {end - start} sec to load an image.')
-plt.show()
+# end = time.time()
+# print(f'it took {end - start} sec to load an image.')
+# plt.show()
+
+# import sys
+# from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+# from PyQt5.QtGui import QIcon, QPixmap
+
+# class App(QWidget):
+
+#     def __init__(self):
+#         super().__init__()
+#         self.title = 'PyQt5 image - pythonspot.com'
+#         # self.left = 10
+#         # self.top = 10
+#         # self.width = 640
+#         # self.height = 480
+#         self.initUI()
+
+#     def initUI(self):
+#         self.setWindowTitle(self.title)
+#         # self.setGeometry(self.left, self.top, self.width, self.height)
+
+#         # Create widget
+#         label = QLabel(self)
+#         pixmap = QPixmap('image.png')
+#         label.setPixmap(pixmap)
+#         self.resize(pixmap.width(),pixmap.height())
+
+#         self.show()
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = App()
+#     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtGui import QPixmap
+# from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+
+# class MainWindow(QMainWindow):
+
+#     def __init__(self):
+#         super(MainWindow, self).__init__()
+#         self.title = "Image Viewer"
+#         self.setWindowTitle(self.title)
+
+#         label = QLabel(self)
+#         pixmap = QPixmap('image.png')
+#         label.setPixmap(pixmap)
+#         self.setCentralWidget(label)
+#         self.resize(pixmap.width(), pixmap.height())
+
+
+# app = QApplication(sys.argv)
+# w = MainWindow()
+# w.show()
+# sys.exit(app.exec_())
+
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+from matplotlib.pyplot import imread
+import sys
+
+app = QtWidgets.QApplication(sys.argv)
+
+#input_image = imread('./images/train/1,100.png')
+#pixmap01 = QtGui.QPixmap.fromImage('./images/train/1,100.png')
+pixmap_image = QtGui.QPixmap('./images/train/1,100.png')
+label_imageDisplay = QtWidgets.QLabel()
+label_imageDisplay.setPixmap(pixmap_image)
+# label_imageDisplay.setAlignment(QtCore.Qt.AlignCenter)
+# label_imageDisplay.setScaledContents(True)
+# label_imageDisplay.setMinimumSize(1,1)
+label_imageDisplay.show()
+sys.exit(app.exec_())
