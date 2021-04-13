62ff0316f1fb6b9eb013efd4d2c58a91a5f7008a
sdiv877 <sdiv877@aucklanduni.ac.nz>
2021-04-14 00:32:35 +1200

    Rows of images instead of just one line, and added labels to images

--- scripts/NewImageDialogTesting.py
+++ scripts/NewImageDialogTesting.py
@@ -1,12 +1,14 @@
 import os
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QSizePolicy
+from PyQt5.QtCore import Qt
+
 from torchvision import datasets, transforms
 
 from PIL import Image
 from PIL.ImageQt import ImageQt
 import io
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -17,6 +19,10 @@ class MainWindow(QtWidgets.QMainWindow):
         print(self.train_dataset)
         print(self.test_dataset)
 
+        # Make a field to keep track of the number of images produced
+        self.imageCount = 0
+        self.newHBox = QtWidgets.QHBoxLayout()
+
         # Make a scroll area and set it as MainWindow's central widgets
         self.scrollArea = QtWidgets.QScrollArea(widgetResizable=True)
         self.setCentralWidget(self.scrollArea)
@@ -24,13 +30,13 @@ class MainWindow(QtWidgets.QMainWindow):
         # Make a content widget with a HBoxLayout that stores out content_widget (that contains all our images)
         content_widget = QtWidgets.QWidget()
         self.scrollArea.setWidget(content_widget)
-        self._lay = QtWidgets.QHBoxLayout(content_widget)
+        self._lay = QtWidgets.QVBoxLayout(content_widget)
 
         # Create an iterator of our training dataset
         self.test_it = iter(self.train_dataset)
 
-        # Create a timer with an interval of 1 second
-        self._timer = QtCore.QTimer(self, interval=1000)
+        # Create a timer with an interval of 1 ms
+        self._timer = QtCore.QTimer(self, interval=1)
 
         # Everytime 1 second is reached , execute the on_timeout function
         self._timer.timeout.connect(self.on_timeout)
@@ -51,17 +57,44 @@ class MainWindow(QtWidgets.QMainWindow):
             qimg = ImageQt(PILImg)
                 
             pixmap = QtGui.QPixmap.fromImage(qimg)
-            self.add_pixmap(pixmap)
+
+            self.add_pixmap(pixmap, label)
 
         # If the iterator reaches its end, StopIteration is raised, and we can stop our timer
-            pass
         except StopIteration:
             self._timer.stop()
 
-    def add_pixmap(self, pixmap):
+    def add_pixmap(self, pixmap, text):
         if not pixmap.isNull():
-            label = QtWidgets.QLabel(pixmap=pixmap)
-            self._lay.addWidget(label)
+            # Our main layout is a VBox
+            # This function adds 20 VBoxes each containing an image and their text, to an HBox
+            # This HBox is then added to our main layout
+            # In this way we simulate creating a table/adding rows (we are just adding HBoxes to a VBox)
+
+            if (self.imageCount % 20 == 0):
+                # Add the finished HBox (that has 20 images/texts) to our main VBox Layout
+                self._lay.addLayout(self.newHBox)
+
+                # Remake the HBox to start adding data again
+                self.newHBox = QtWidgets.QHBoxLayout()
+
+            # Create a new VBox to store the current image and label
+            imgAndLabelBox = QtWidgets.QVBoxLayout()
+
+            # Create the widgets for our image and label, using the vals the add_pixmap function was passed
+            imgLabel = QtWidgets.QLabel(pixmap=pixmap)
+            imgLabel.setAlignment(Qt.AlignCenter)
+            textLabel = QtWidgets.QLabel(text=str(text))
+            textLabel.setAlignment(Qt.AlignCenter)
+
+            # Add the image and text to the VBox
+            imgAndLabelBox.addWidget(imgLabel)
+            imgAndLabelBox.addWidget(textLabel)
+
+            # Add the VBox to our current HBox (row)
+            self.newHBox.addLayout(imgAndLabelBox)
+
+            self.imageCount += 1
 
     def loadMNIST(self):
             # MNIST Dataset
